a33354b34ddb8a7a0ae388544b47de456e2b2fd1
Chunhoyoung <ho723@naver.com>
2021-03-25 15:24:43 +0900

    미디어경로지정

--- config/settings.py
+++ config/settings.py
@@ -117,3 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#미디어경로 지정
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
