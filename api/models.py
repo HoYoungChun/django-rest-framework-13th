84f3e805d44e413b5cc6d310e3996c325d719b62
Chunhoyoung <ho723@naver.com>
2021-03-25 15:16:56 +0900

    Post랑 User ForeignKey연결

--- api/models.py
+++ api/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-
-# Create your models here.
-
 class User(models.Model):
     # 닉네임, 이메일, 전화번호로 로그인하기 때문에 Unique해야
     nickname = models.CharField(max_length=100, verbose_name="닉네임", unique=True)
@@ -16,6 +13,17 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="가입일자")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="업데이트일자")
 
+    def __str__(self):
+        return str(self.pk) +' '+self.nickname
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    content = models.TextField(verbose_name="소개")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="업로드일자")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="업데이트일자")
+
+    def __str__(self):
+        return str(self.pk) +' '+self.title
