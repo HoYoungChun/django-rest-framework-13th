7089c9314b855f803ed4da1b51e1b5c75ff3a1de
Chunhoyoung <ho723@naver.com>
2021-04-01 02:23:08 +0900

    PostSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 from .models import *
 
+class PostSerializer(serializers.ModelSerializer):
+    heart_post = HeartSerializer(many=True, read_only=True)
+    file_post = FileSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'created_at', 'updated_at']
+
 class UserSerializer(serializers.ModelSerializer):
     _following = FollowSerializer(many=True, read_only=True)
     _followed = FollowSerializer(many=True, read_only=True)
