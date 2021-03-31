aa7ac622c6fa9db5dccbc20f8e3940b455f60f01
Chunhoyoung <ho723@naver.com>
2021-04-01 02:54:55 +0900

    User url connect

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from api.models import *
+from api.serializers import *
 
 # Create your views here.
+@csrf_exempt
+def user_list(request):
+    """
+    List all users, or create a new user.
+    """
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
