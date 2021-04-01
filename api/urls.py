aa537476bae4ce18c8c51ad5cb6e637275143b06
Chunhoyoung <ho723@naver.com>
2021-04-01 18:44:46 +0900

    POST방식도 확인

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('users/', views.UserList.as_view())
+    path('users/', views.UserList.as_view()),
+    path('posts/', views.PostList.as_view()),
+    path('files/', views.FileList.as_view()),
+    path('follows/', views.FollowList.as_view()),
+    path('hearts/', views.HeartList.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -16,3 +16,51 @@ class UserList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PostList(APIView):
+    def get(self, request, format=None):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    def post(self, request, format=None):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FileList(APIView):
+    def get(self, request, format=None):
+        files = File.objects.all()
+        serializer = FileSerializer(files, many=True)
+        return Response(serializer.data)
+    def post(self, request, format=None):
+        serializer = FileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FollowList(APIView):
+    def get(self, request, format=None):
+        follows = Follow.objects.all()
+        serializer = FollowSerializer(follows, many=True)
+        return Response(serializer.data)
+    def post(self, request, format=None):
+        serializer = FollowSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class HeartList(APIView):
+    def get(self, request, format=None):
+        hearts = Heart.objects.all()
+        serializer = HeartSerializer(hearts, many=True)
+        return Response(serializer.data)
+    def post(self, request, format=None):
+        serializer = HeartSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
