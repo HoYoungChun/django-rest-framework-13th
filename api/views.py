a273d6cf4bf651472c3aa3da97d5e8cd0d4ae0a8
HoYoungChun <ho723@naver.com>
2021-05-13 02:14:09 +0900

    Filter작성

--- api/views.py
+++ api/views.py
@@ -5,10 +5,31 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import FilterSet, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Q
 
+class UserFilter(FilterSet):
+    nickname = filters.CharFilter(field_name='nickname', lookup_expr="icontains")
+    is_hy1 = filters.BooleanFilter(method='filter_is_hy1')
+
+    class Meta:
+        model = User
+        fields = ['nickname']
+
+    def filter_is_hy1(self, queryset, nickname, value):
+        filtered_queryset = queryset.filter(nickname="hy1")
+        filtered_queryset2 = queryset.filter(~Q(nickname="hy1"))
+        if value == True:
+            return filtered_queryset
+        else:
+            return filtered_queryset2
+        
 class UserViewSet(viewsets.ModelViewSet):
-	serializer_class = UserSerializer
-	queryset = User.objects.all()
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = UserFilter
 
 class PostViewSet(viewsets.ModelViewSet):
 	serializer_class = PostSerializer
--- config/settings.py
+++ config/settings.py
@@ -42,8 +42,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'api.apps.ApiConfig',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
